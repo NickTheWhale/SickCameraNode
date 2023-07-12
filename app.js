93b807686fed1b6169a403135850d52c1c99d8a7
NickTheWhale <loehrketurkey7@gmail.com>
2023-07-12 15:10:22 -0500

    bare tcpsocket -> websocket for qt connection

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const net = require('net');
+const WebSocket = require('ws')
 const http = require('http');
 const ejs = require('ejs');
 const dotenv = require('dotenv');
@@ -10,69 +10,27 @@ const HTTP_PORT = process.env.HTTP_PORT || 8080;
 const TCP_ADDRESS = process.env.TCP_ADDRESS || "127.0.0.1";
 const TCP_PORT = process.env.TCP_PORT || 3000;
 
-var g_imageData = null;
 var g_latestImageData = null;
 var g_count = null;
 
-function handleData(data) {
-    try {
-        var offset = 0;
-        if (isPacketStart(data, offset)) {
-            offset += 8;
-            const height = data.readUInt32LE(offset); offset += 4;
-            const width = data.readUInt32LE(offset); offset += 4;
-            const number = data.readUInt32LE(offset); offset += 4;
-            const time = data.readBigUInt64LE(offset); offset += 8;
-            const size = data.readBigUInt64LE(offset); offset += 8;
-            //console.log(`height: ${height}, width: ${width}, number: ${number}, time: ${time}, size: ${size}`);
-            g_count = number;
-            g_imageData = Buffer.allocUnsafe(Number(size));
-            data.copy(g_imageData, 0, offset);
-        }
-        if (g_imageData) {
-            const remainingSize = g_imageData.length - g_imageData.byteOffset - g_imageData.length;
-            const dataSize = Math.min(data.length, remainingSize);
-            data.copy(g_imageData, g_imageData.byteOffset + g_imageData.length - remainingSize, 0, dataSize);
 
-            if (dataSize < remainingSize) {
-                g_imageData = g_imageData.slice(0, g_imageData.byteOffset + g_imageData.length - remainingSize + dataSize);
-            } else {
-                g_latestImageData = Buffer.copyBytesFrom(g_imageData);
-                g_imageData = null;
-            }
-        }
-    }
-    catch (error) {
-        console.error(error?.message ?? "handleData() failed");
-    }
-}
+const wss = new WebSocket.Server({ port: TCP_PORT });
+wss.on('connection', (ws) => {
+    console.log('Connected to a client');
 
-function isPacketStart(data, offset) {
-    ret = false
-    if (data.byteLength >= 8) {
-        ret = data[offset++] === 0
-            && data[offset++] === 255
-            && data[offset++] === 0
-            && data[offset++] === 255
-            && data[offset++] === 0
-            && data[offset++] === 255
-            && data[offset++] === 0
-            && data[offset++] === 255;
-    }
-    return ret;
-}
-
-const tcpServer = net.createServer((socket) => {
-    socket.on('data', handleData);
-    socket.on('error', (err) => { console.error('tcp socket error', err); });
-});
-
-tcpServer.on('error', (err) => {
-    console.error('tcp server error:', err);
-});
-
-tcpServer.listen(TCP_PORT, TCP_ADDRESS, () => {
-    console.log(`tcp server listening at ${TCP_ADDRESS}:${TCP_PORT}`);
+    ws.on('message', (message) => {
+        offset = 8;
+        const height = message.readUInt32LE(offset); offset += 4;
+        const width = message.readUInt32LE(offset); offset += 4;
+        const number = message.readUInt32LE(offset); offset += 4;
+        const time = message.readBigUInt64LE(offset); offset += 8;
+        const size = message.readBigUInt64LE(offset); offset += 8;
+        //console.log(`height: ${height}, width: ${width}, number: ${number}, time: ${time}, size: ${size}`);
+        g_latestImageData = Buffer.allocUnsafe(Number(size));
+        message.copy(g_latestImageData, 0, offset, message.length);
+        g_count = number;
+        console.log(g_latestImageData);
+    });
 });
 
 const httpServer = http.createServer((req, res) => {
